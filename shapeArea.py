b4a7b5d90097cd66772f3bb7d4e51949a7b0722d
Renso Guilalas <rensoglorenzo@gmail.com>
2023-10-11 23:48:29 +0100

    Solved shapeArea.py

---
+++ shapeArea.py
@@ -0,0 +1,8 @@
+def solution(n):
+    #quadratic sequence formula
+    return 2 * (n ** 2) - (2 * n) + 1
+
+if __name__ == "__main__":
+    print(solution(1))
+    print(solution(2))
+    print(solution(3))
