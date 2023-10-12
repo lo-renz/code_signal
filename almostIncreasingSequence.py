145cf99bdd95382ef41d81fd20d979ff296717f8
Renso Guilalas <renso.guilalas2@mail.dcu.ie>
2023-10-12 12:53:40 +0100

    Working on strictly increaasing question.

--- almostIncreasingSequence.py
+++ almostIncreasingSequence.py
@@ -5,21 +5,39 @@ def solution(sequence):
 
     # While loop to find first instance of left number greater than right.
     # Traversing left to right.
+    # [1, 2, 3, 4, 3, 6]
+    # [0, 1, 2, 3, 4, 5]
+    # i = 3
+
+    # [10, 1, 2, 3, 4, 5]
+    # [0, 1, 2, 3, 4, 5]
+    # i = 0
     i = 0
-    while sequence[i+1] > sequence[i]:
+    while sequence[i] < sequence[i+1]:
         i += 1
 
-    # [3, 5, 67, 98, 3]
-    # [0, 1, 2, 3, 4, 5]
     # Traversing right to left
-    #i = len(sequence) - 1
-    #while sequence[i] > sequence[i-1]:
-    #    i -= 1
-    #return i
+    # [1, 2, 3, 4, 3, 6]
+    # [0, 1, 2, 3, 4, 5]
+    # j = 4
+
+    # [10, 1, 2, 3, 4, 5]
+    # [0, 1, 2, 3, 4, 5]
+    # j = 1
+    j = len(sequence) - 1
+    while sequence[j] > sequence[j-1]:
+        j -= 1
 
     # When the first instance is found, remove it from the list.
-    sequence.pop(i)
-    #return sequence
+    if j > i:
+        if sequence[j] < sequence[j-1]:
+            sequence.pop(j-1)
+        else:
+            sequence.pop(j)
+    else:
+        sequence.pop(i)
+
+    return sequence
 
     # Loop through the list again to check if there is another instance where the right number is less than the left.
     # [1, 2, 3, 4, 6]
@@ -37,5 +55,5 @@ def solution(sequence):
 
 
 if __name__ == "__main__":
-    print(solution([1, 2, 3, 4, 3, 6]))
+    print(solution([10, 1, 2, 3, 4, 5]))
     #print(solution([3, 5, 67, 98, 3]))
