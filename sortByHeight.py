a995106d4e4b70f242d9aee38e1cb6333734d85a
Renso Guilalas <rensoglorenzo@gmail.com>
2023-10-13 00:22:36 +0100

    Working on sortByHeight.py

---
+++ sortByHeight.py
@@ -0,0 +1,14 @@
+def solution(a):
+    # swapping variables
+    # tmp = a
+    # a = b
+    # b = tmp
+    i = 0
+    j = 1
+    while j < len(a):
+    return a
+
+
+if __name__ == "__main__":
+    a = [-1, 150, 190, 170, -1, -1, 160, 180]
+    print(solution(a))
