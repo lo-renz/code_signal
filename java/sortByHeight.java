6c91efa06eac0f24f64d4a0c046fa2500671bb01
Renso Guilalas <rensoglorenzo@gmail.com>
2023-10-14 22:02:35 +0100

    Working on sortByHeight.java

--- java/sortByHeight.java
+++ java/sortByHeight.java
@@ -1,43 +1,30 @@
 public class sortByHeight {
     /*
         Possible solution:
-            Sort the list using linar search. If the numer-1 is encountered then skip it.
+            Sort the list using linar search. If the number -1 is encountered then skip it.
     */
 
-    int[] sorting_list(int[] a) {
-        int[] to_sort_list = new int[a.length];
-
-        for(int i = 0; i < a.length; i++) {
-            if(a[i] != -1) {
-                to_sort_list[i] = a[i];
-            }
-        }
-
-        return to_sort_list;
-    }
-
     int[] solution(int[] a) {
-        int[] list_to_sort = new int[a.length];
-
-        int i = 0;
-        for(int j = 1; i < a.length; i++) {
+        int n = a.length;
+        for(int i = 1; i < n; i++) {
+            int key = a[i];
+            int j = i - 1;
+        
+            while(j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j = j - 1;
+            }
+            a[j + 1] = key;
         }
+        return a;
     }
     
     public static void main(String[] args) {
-        int[] a = new int[]{-1, 150, 190, 170, -1, -1, 160, 180};
+        int[] a = new int[]{ 150, 190, 170, 160, 180};
         sortByHeight test = new sortByHeight();
-        
-        // testing for index list
-        /*int[] index_list = test.index_search(a);
-        for(int i = 0; i < index_list.length; i++) {
-            System.out.println(index_list[i]);
-        }*/
-
-        // testing for sorting list
-        int[] list_to_sort = test.sorting_list(a);
-        for(int i = 0; i < list_to_sort.length; i++) {
-            System.out.println(list_to_sort[i]);
+        int[] sorted_list = test.solution(a);
+        for(int i = 0; i < a.length; i++) {
+            System.out.println(sorted_list[i]);
         }
     }
 }
